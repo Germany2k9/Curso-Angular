5d459a64aeeae7bf78e6451b21329181bc6721d6
German Aliaga <germanyp20l@gmail.com>
2024-07-03 13:12:28 +0200

    Output desde el DOM para recuperar el txt de lo ingresado por el usuario.

--- src/app/hijo/hijo.component.ts
+++ src/app/hijo/hijo.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-hijo',
@@ -6,6 +6,11 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./hijo.component.css']
 })
 export class HijoComponent {
-  // Este es el emensaje que recibe desde el padre 
-   @Input() recibeHijo?: string;
+  @Output() mensajeDesdeHijo = new EventEmitter<string>() 
+
+   mensaje : string= '';
+
+  enviarMensaje(){
+    this.mensajeDesdeHijo.emit(this.mensaje)
+  }
 }
--- src/app/padre/padre.component.ts
+++ src/app/padre/padre.component.ts
@@ -8,7 +8,15 @@ import { Component } from '@angular/core';
 export class PadreComponent {
 
  valorContador: number = 0;
- mensajePadre= "Ojala Messi llegue al proximo mundial."; 
+//  mensajePadre= "Ojala Messi llegue al proximo mundial."; 
+
+  mensajeRecibido: string= '';
+
+  recibirMensaje($event: string){
+
+    this.mensajeRecibido = $event;
+  }
+
 
  incrementar(){
   this.valorContador++;
