34963ec6eb650c5cf045ebb489f85059a56bc82e
German Aliaga <germanyp20l@gmail.com>
2024-07-04 13:33:20 +0200

    Servicios : pibot de informacion de 2 componentes donde comparten logica para mostrar informacion.

--- src/app/padre/padre.component.ts
+++ src/app/padre/padre.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ServicioFamiliarService } from '../servicio-familiar.service';
 
 @Component({
   selector: 'app-padre',
   templateUrl: './padre.component.html',
   styleUrls: ['./padre.component.css']
 })
-export class PadreComponent {
+export class PadreComponent implements OnInit{
+  
+  nombre?: string;
+
+  constructor(
+    private _servicioFamiliar : ServicioFamiliarService
+  ) { }
+
+   
+
+  ngOnInit(): void {
+    this._servicioFamiliar.setHermanoGrande('Juan');
+    this.nombre = this._servicioFamiliar.getHermanoGrande();
+
+      
+  }
+
+    saludar(){
+      this._servicioFamiliar.saludar(this._servicioFamiliar.getHermanoPequeno() || '');
+    }
+
+
+    preguntar(){
+      console.log(this._servicioFamiliar.preguntarPorHijo());
+    }
 
  valorContador: number = 0;
 //  mensajePadre= "Ojala Messi llegue al proximo mundial."; 
